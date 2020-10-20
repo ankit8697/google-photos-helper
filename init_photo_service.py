b504033a14edc6be8a57ad7c7e54463cbaa8577b
ankit8697 <ankitsanghi@gmail.com>
2020-10-20 21:09:24 +0530

    updated scopes

--- init_photo_service.py
+++ init_photo_service.py
@@ -4,8 +4,7 @@ from Google import create_service
 API_NAME = 'photoslibrary'
 API_VERSION = 'v1'
 CLIENT_SECRET_FILE = 'credentials.json'
-SCOPES = ['https://www.googleapis.com/auth/photoslibrary',
-          'https://www.googleapis.com/auth/photoslibrary.sharing']
+SCOPES = ['https://www.googleapis.com/auth/photoslibrary']
 
 def connect_to_api():
     service = create_service(CLIENT_SECRET_FILE,API_NAME, API_VERSION, SCOPES)   
